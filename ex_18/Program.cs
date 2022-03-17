cb604b95d01cc3bc1d8cecb8514c03ea782cdefa
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-17 13:45:58 +0500

    Ex 18 compl

--- ex_18/Program.cs
+++ ex_18/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Введите число");
+            string number_string = Console.ReadLine();
+            while (number_string.Length !=5)
+                {
+                    Console.WriteLine("Вы ввели не пятизначное число. Попробуйте еще раз.");
+                    number_string = Console.ReadLine();
+                }
+            Palindrom(number_string);
+
+            void Palindrom(string num)
+            {
+                if(num[0]==num[4] && num[1]==num[3])
+                {
+                    Console.WriteLine("Заданное число является палиндромом");
+                }
+                else{Console.WriteLine("Заданное число не является палиндромом");}
+
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
