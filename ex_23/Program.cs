03af8e3b0f3839e2b83373515677ec61177515ce
Constantin <grishanoff.konstantin@yandex.ru>
2022-03-18 13:16:03 +0500

    Ex 23 coml

---
+++ ex_23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        
+
+        {
+            Console.WriteLine("Введите число:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Cub(n);
+
+
+            void Cub(int number)
+            
+            {
+                int a;
+                for (int i=0; i<number; i++)
+                {
+                    a = i*i*i;
+                    Console.WriteLine($"{i} _|_ {a}");
+
+
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
